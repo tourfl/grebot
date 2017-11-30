dfd081b0745175efceea330e17c9dc645e686210
Raphaël Dein <raphaeldein@gmail.com>
2017-11-30 10:55:31 +0100

    has a discussion started?

--- gre.py
+++ gre.py
@@ -1,13 +1,20 @@
+users = {"18": "test"}
+
 
 def process(in_message, user_id):
-    out_message = user_id
+    out_message = in_message
+
+    try:
+        out_message = users[str(user_id)]
+    except KeyError:
+        out_message = "not started any discussion"
 
     return out_message
 
 
 def test():
     input = "RU"
-    id = "1A"
+    id = 46
 
     output = process(input, id)
 
