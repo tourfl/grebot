e0c776c69983d95b56b5ec75ae0e03002adc6403
Raphaël Dein <raphaeldein@gmail.com>
2017-11-30 10:49:40 +0100

    get user id

--- gre.py
+++ gre.py
@@ -1,4 +1,17 @@
 
 def process(in_message, user_id):
-    out_message = in_message
+    out_message = user_id
+
     return out_message
+
+
+def test():
+    input = "RU"
+    id = "1A"
+
+    output = process(input, id)
+
+    print(output)
+
+if __name__ == '__main__':
+    test()
